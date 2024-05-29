c9a5931912a68055758cf3692fe6a6a580236b1f
Drona-Srivastava <srivastavadrona@gmail.com>
2024-05-29 12:39:59 +0530

    Hamburger Menu added

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-dkj
+function toggleMenu() {
+    const menu = document.querySelector(".ham-links");
+    const icon = document.querySelector(".ham-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
